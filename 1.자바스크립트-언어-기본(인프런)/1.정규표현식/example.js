77d6cf43e16660520be73cdbca7be85657dd28f9
정지원 <>
2021-06-19 02:42:00 +0900

    6/18 (금) 자바스크립트 정규표현식

---
+++ 1.자바스크립트-언어-기본(인프런)/1.정규표현식/example.js
@@ -0,0 +1,46 @@
+pattern = /a/;
+
+//    1. 기본 문법
+//추출
+console.log(pattern.exec('abcde'));
+
+//테스트
+console.log(pattern.test('abcde'));
+
+//    2.문자열의 정규표현식
+var str = 'abced';
+console.log(str.match(pattern));
+
+//문자열 치환
+console.log(str.replace(pattern,'A'));
+
+
+//    3. 옵션
+var str = 'abcdeaA';
+var patternX = /a/;
+console.log(str.match(patternX));
+
+//소문자,대문자 match
+var patternI = /a/i;
+console.log(str.match(patternI))
+
+//여러개 match
+var patternG = /a/g;
+console.log(str.match(patternG))
+
+
+//    4. 캡쳐
+pattern = /(\w+)\s(\w+)/;
+str = "coding everyday";
+var result = str.replace(pattern,'$2,$1');
+console.log(result);
+
+
+//    5. 치환 
+//url->html tag 예제
+var urlPattern = /\b(?:https?):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*/gim;
+var content = '제 블로그 주소는 http://esthevely.tistory.com 입니다. 네이버 : http://naver.com 입니다. ';
+var result = content.replace(urlPattern, function(url){
+    return '<a href="'+url+'">'+url+'</a>';
+});
+console.log(result);
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Study-JavaScript
 JavaScript 기초 문법 공부
+
+인프런 무료강좌
+1.자바스크립트 언어 기본 (Egoing Lee)
+2.웹브라우저 Javascript (Egoing Lee)
+3.JavaScript ES6+ 제대로 알아보기 (정재남)
+4.웹 게임을 만들며 배우는 자바스크림트 (조현영)
+
+책 공부
+5.코어 자바스크립트
