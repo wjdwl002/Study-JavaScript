7b0e02ba7fb944fe158b11f35f5511ed50fe0ab6
wjdwl002 <wjdwl001@khu.ac.kr>
2021-09-27 23:20:27 +0900

    07-03 multi-page

---
+++ 7. Do-it 프로그레시브 웹앱 만들기/07/07-03/src/components/main_page.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <v-container>
+            <p class="display-1 my-4">Main Page</p>
+            <v-divider></v-divider>
+            <p class="display-4 my-4">This Is Main Page</p>
+            <div class="text-center">
+                <v-btn fab large class="mt-5" color="purple" dark to="/sub">
+                    <v-icon>mdi-arrow-right</v-icon>
+                </v-btn>
+            </div>
+        </v-container>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
